99fef06ad63141f66c31d9187c5a0b2fefdf8a1f
Leslie Taffe <92220727+XresuriX@users.noreply.github.com>
2026-02-24 20:13:13 -0500

    test config

--- vite.config.ts
+++ vite.config.ts
@@ -16,8 +16,8 @@ export default defineConfig({
   },
   
   server: {
-    host: '0.0.0.0',
-    port: 5173,
+    host: true,
+    port: 3000,
     cors: { origin: '*', credentials: true }
   },
   
